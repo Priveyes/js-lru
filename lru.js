d7bcbd1934fa5ac355b4b8045332bf31e33f2481
Pascal Marois <meteo66240@free.fr>
2017-03-26 14:16:06 +0200

    Update lru.js
    Migrate to class

--- lru.js
+++ lru.js
@@ -1,7 +1,7 @@
 /**
- * A doubly linked list-based Least Recently Used (LRU) cache. Will keep most
- * recently used items while discarding least recently used items when its limit
- * is reached.
+ * A doubly linked list-based Least Recently Used (LRU) cache.
+ * Will keep most recently used items while discarding least recently
+ * used items when its limit is reached.
  *
  * Licensed under MIT. Copyright (c) 2010 Rasmus Andersson <http://hunch.se/>
  * See README.md for details.
@@ -15,291 +15,342 @@
  *      |______| <= older.|______| <= older.|______| <= older.|______|
  *
  *  removed  <--  <--  <--  <--  <--  <--  <--  <--  <--  <--  <--  added
+ *
+ * // With AMDLD:
+ * define(['lru'], function(lru) {
+ * let c = new lru.LRUMap(3)
+ * ...
  */
-(function(g,f){
-  const e = typeof exports == 'object' ? exports : typeof g == 'object' ? g : {};
-  f(e);
-  if (typeof define == 'function' && define.amd) { define('lru', e); }
-})(this, function(exports) {
-
-const NEWER = Symbol('newer');
-const OLDER = Symbol('older');
-
-function LRUMap(limit, entries) {
-  if (typeof limit !== 'number') {
-    // called as (entries)
-    entries = limit;
-    limit = 0;
-  }
-
-  this.size = 0;
-  this.limit = limit;
-  this.oldest = this.newest = undefined;
-  this._keymap = new Map();
-
-  if (entries) {
-    this.assign(entries);
-    if (limit < 1) {
-      this.limit = this.size;
+(((g, f) => {
+    const e = typeof exports == 'object' ? exports : typeof g == 'object' ? g : {};
+    f(e);
+    if (typeof define == 'function' && define.amd) {
+        define('lru', e);
     }
-  }
-}
+}))(this, exports => {
+    const NEWER = Symbol('newer');
+    const OLDER = Symbol('older');
 
-exports.LRUMap = LRUMap;
+    /**
+     * You can use an Array (or any iterable) with [key, value] pairs to
+     * set up the initial data in the Map:
+     const map = new Map([
+     [ 1, 'one' ],
+     [ 2, 'two' ],
+     [ 3, 'three' ], // trailing comma is ignored
+     ]);
+     * @param limit
+     * @param entries
+     * @constructor
+     */
+    class LRUMap {
+        constructor(limit, entries) {
+            if (typeof limit !== 'number') {
+                // called as (entries)
+                entries = limit;
+                limit = 0;
+            }
 
-function Entry(key, value) {
-  this.key = key;
-  this.value = value;
-  this[NEWER] = undefined;
-  this[OLDER] = undefined;
-}
+            this.size = 0;
+            this.limit = limit;
+            this.oldest = this.newest = undefined;
+            this._keymap = new Map();
 
+            if (entries) {
+                this.assign(entries);
+                if (limit < 1) {
+                    this.limit = this.size;
+                }
+            }
+        }
 
-LRUMap.prototype._markEntryAsUsed = function(entry) {
-  if (entry === this.newest) {
-    // Already the most recenlty used entry, so no need to update the list
-    return;
-  }
-  // HEAD--------------TAIL
-  //   <.older   .newer>
-  //  <--- add direction --
-  //   A  B  C  <D>  E
-  if (entry[NEWER]) {
-    if (entry === this.oldest) {
-      this.oldest = entry[NEWER];
+        _markEntryAsUsed(entry) {
+            if (entry === this.newest) {
+                // Already the most recenlty used entry, so no need to update the list
+                return;
+            }
+            // HEAD--------------TAIL
+            //   <.older   .newer>
+            //  <--- add direction --
+            //   A  B  C  <D>  E
+            if (entry[NEWER]) {
+                if (entry === this.oldest) {
+                    this.oldest = entry[NEWER];
+                }
+                entry[NEWER][OLDER] = entry[OLDER]; // C <-- E.
+            }
+            if (entry[OLDER]) {
+                entry[OLDER][NEWER] = entry[NEWER]; // C. --> E
+            }
+            entry[NEWER] = undefined; // D --x
+            entry[OLDER] = this.newest; // D. --> E
+            if (this.newest) {
+                this.newest[NEWER] = entry; // E. <-- D
+            }
+            this.newest = entry;
+        }
+
+        assign(entries) {
+            let entry;
+            let limit = this.limit || Number.MAX_VALUE;
+            this._keymap.clear();
+            let it = entries[Symbol.iterator]();
+            for (let itv = it.next(); !itv.done; itv = it.next()) {
+                let e = new Entry(itv.value[0], itv.value[1]);
+                this._keymap.set(e.key, e);
+                if (entry) {
+                    entry[NEWER] = e;
+                    e[OLDER] = entry;
+                } else {
+                    this.oldest = e;
+                }
+                entry = e;
+                if (limit-- == 0) {
+                    throw new Error('overflow');
+                }
+            }
+            this.newest = entry;
+            this.size = this._keymap.size;
+        }
+
+        get(key) {
+            // First, find our cache entry
+            const entry = this._keymap.get(key);
+            if (!entry) return; // Not cached. Sorry.
+            // As <key> was found in the cache, register it as being requested recently
+            this._markEntryAsUsed(entry);
+            return entry.value;
+        }
+
+        set(key, value) {
+            let entry = this._keymap.get(key);
+
+            if (entry) {
+                // update existing
+                entry.value = value;
+                this._markEntryAsUsed(entry);
+                return this;
+            }
+
+            // new entry
+            this._keymap.set(key, (entry = new Entry(key, value)));
+
+            if (this.newest) {
+                // link previous tail to the new tail (entry)
+                this.newest[NEWER] = entry;
+                entry[OLDER] = this.newest;
+            } else {
+                // we're first in -- yay
+                this.oldest = entry;
+            }
+
+            // add new entry to the end of the linked list -- it's now the freshest entry.
+            this.newest = entry;
+            ++this.size;
+            if (this.size > this.limit) {
+                // we hit the limit -- remove the head
+                this.shift();
+            }
+
+            return this;
+        }
+
+        shift() {
+            // todo: handle special case when limit == 1
+            const entry = this.oldest;
+            if (entry) {
+                if (this.oldest[NEWER]) {
+                    // advance the list
+                    this.oldest = this.oldest[NEWER];
+                    this.oldest[OLDER] = undefined;
+                } else {
+                    // the cache is exhausted
+                    this.oldest = undefined;
+                    this.newest = undefined;
+                }
+                // Remove last strong reference to <entry> and remove links from the purged
+                // entry being returned:
+                entry[NEWER] = entry[OLDER] = undefined;
+                this._keymap.delete(entry.key);
+                --this.size;
+                return [entry.key, entry.value];
+            }
+        }
+
+        // ----------------------------------------------------------------------------
+        // Following code is optional and can be removed without breaking the core
+        // functionality.
+
+        find(key) {
+            let e = this._keymap.get(key);
+            return e ? e.value : undefined;
+        }
+
+        has(key) {
+            return this._keymap.has(key);
+        }
+
+        clear() {
+            // Not clearing links should be safe, as we don't expose live links to user
+            this.oldest = this.newest = undefined;
+            this.size = 0;
+            this._keymap.clear();
+        }
+
+        keys() {
+            return new KeyIterator(this.oldest);
+        }
+
+        values() {
+            return new ValueIterator(this.oldest);
+        }
+
+        entries() {
+            return this;
+        }
+
+        forEach(fun, thisObj) {
+            if (typeof thisObj !== 'object') {
+                thisObj = this;
+            }
+            let entry = this.oldest;
+            while (entry) {
+                fun.call(thisObj, entry.value, entry.key, this);
+                entry = entry[NEWER];
+            }
+        }
+
+        /** Returns a JSON (array) representation */
+        toJSON() {
+            let s = new Array(this.size);
+            let i = 0;
+            let entry = this.oldest;
+            while (entry) {
+                s[i++] = {key: entry.key, value: entry.value};
+                entry = entry[NEWER];
+            }
+            return s;
+        }
+
+        /** Returns a String representation */
+        toString() {
+            let s = '';
+            let entry = this.oldest;
+            while (entry) {
+                s += `${String(entry.key)}:${entry.value}`;
+                entry = entry[NEWER];
+                if (entry) {
+                    s += ' < ';
+                }
+            }
+            return s;
+        }
     }
-    entry[NEWER][OLDER] = entry[OLDER]; // C <-- E.
-  }
-  if (entry[OLDER]) {
-    entry[OLDER][NEWER] = entry[NEWER]; // C. --> E
-  }
-  entry[NEWER] = undefined; // D --x
-  entry[OLDER] = this.newest; // D. --> E
-  if (this.newest) {
-    this.newest[NEWER] = entry; // E. <-- D
-  }
-  this.newest = entry;
-};
 
-LRUMap.prototype.assign = function(entries) {
-  let entry, limit = this.limit || Number.MAX_VALUE;
-  this._keymap.clear();
-  let it = entries[Symbol.iterator]();
-  for (let itv = it.next(); !itv.done; itv = it.next()) {
-    let e = new Entry(itv.value[0], itv.value[1]);
-    this._keymap.set(e.key, e);
-    if (!entry) {
-      this.oldest = e;
-    } else {
-      entry[NEWER] = e;
-      e[OLDER] = entry;
+    exports.LRUMap = LRUMap;
+
+    function Entry(key, value) {
+        this.key = key;
+        this.value = value;
+        this[NEWER] = undefined;
+        this[OLDER] = undefined;
     }
-    entry = e;
-    if (limit-- == 0) {
-      throw new Error('overflow');
+
+    LRUMap.prototype[Symbol.iterator] = function () {
+        return new EntryIterator(this.oldest);
+    };
+
+    /** TODO New way */
+    function mapToJson(map) {
+        return JSON.stringify([...map]);
     }
-  }
-  this.newest = entry;
-  this.size = this._keymap.size;
-};
-
-LRUMap.prototype.get = function(key) {
-  // First, find our cache entry
-  var entry = this._keymap.get(key);
-  if (!entry) return; // Not cached. Sorry.
-  // As <key> was found in the cache, register it as being requested recently
-  this._markEntryAsUsed(entry);
-  return entry.value;
-};
-
-LRUMap.prototype.set = function(key, value) {
-  var entry = this._keymap.get(key);
-
-  if (entry) {
-    // update existing
-    entry.value = value;
-    this._markEntryAsUsed(entry);
-    return this;
-  }
-
-  // new entry
-  this._keymap.set(key, (entry = new Entry(key, value)));
-
-  if (this.newest) {
-    // link previous tail to the new tail (entry)
-    this.newest[NEWER] = entry;
-    entry[OLDER] = this.newest;
-  } else {
-    // we're first in -- yay
-    this.oldest = entry;
-  }
-
-  // add new entry to the end of the linked list -- it's now the freshest entry.
-  this.newest = entry;
-  ++this.size;
-  if (this.size > this.limit) {
-    // we hit the limit -- remove the head
-    this.shift();
-  }
-
-  return this;
-};
-
-LRUMap.prototype.shift = function() {
-  // todo: handle special case when limit == 1
-  var entry = this.oldest;
-  if (entry) {
-    if (this.oldest[NEWER]) {
-      // advance the list
-      this.oldest = this.oldest[NEWER];
-      this.oldest[OLDER] = undefined;
-    } else {
-      // the cache is exhausted
-      this.oldest = undefined;
-      this.newest = undefined;
+    function jsonToMap(jsonStr) {
+        return new Map(JSON.parse(jsonStr));
     }
-    // Remove last strong reference to <entry> and remove links from the purged
-    // entry being returned:
-    entry[NEWER] = entry[OLDER] = undefined;
-    this._keymap.delete(entry.key);
-    --this.size;
-    return [entry.key, entry.value];
-  }
-};
 
-// ----------------------------------------------------------------------------
-// Following code is optional and can be removed without breaking the core
-// functionality.
+    LRUMap.prototype['delete'] = function (key) {
+        const entry = this._keymap.get(key);
+        if (!entry) return;
+        this._keymap.delete(entry.key);
+        if (entry[NEWER] && entry[OLDER]) {
+            // relink the older entry with the newer entry
+            entry[OLDER][NEWER] = entry[NEWER];
+            entry[NEWER][OLDER] = entry[OLDER];
+        } else if (entry[NEWER]) {
+            // remove the link to us
+            entry[NEWER][OLDER] = undefined;
+            // link the newer entry to head
+            this.oldest = entry[NEWER];
+        } else if (entry[OLDER]) {
+            // remove the link to us
+            entry[OLDER][NEWER] = undefined;
+            // link the newer entry to head
+            this.newest = entry[OLDER];
+        } else {// if(entry[OLDER] === undefined && entry.newer === undefined) {
+            this.oldest = this.newest = undefined;
+        }
 
-LRUMap.prototype.find = function(key) {
-  let e = this._keymap.get(key);
-  return e ? e.value : undefined;
-};
+        this.size--;
+        return entry.value;
+    };
 
-LRUMap.prototype.has = function(key) {
-  return this._keymap.has(key);
-};
+    class EntryIterator {
+        constructor(oldestEntry) {
+            this.entry = oldestEntry;
+        }
 
-LRUMap.prototype['delete'] = function(key) {
-  var entry = this._keymap.get(key);
-  if (!entry) return;
-  this._keymap.delete(entry.key);
-  if (entry[NEWER] && entry[OLDER]) {
-    // relink the older entry with the newer entry
-    entry[OLDER][NEWER] = entry[NEWER];
-    entry[NEWER][OLDER] = entry[OLDER];
-  } else if (entry[NEWER]) {
-    // remove the link to us
-    entry[NEWER][OLDER] = undefined;
-    // link the newer entry to head
-    this.oldest = entry[NEWER];
-  } else if (entry[OLDER]) {
-    // remove the link to us
-    entry[OLDER][NEWER] = undefined;
-    // link the newer entry to head
-    this.newest = entry[OLDER];
-  } else {// if(entry[OLDER] === undefined && entry.newer === undefined) {
-    this.oldest = this.newest = undefined;
-  }
-
-  this.size--;
-  return entry.value;
-};
-
-LRUMap.prototype.clear = function() {
-  // Not clearing links should be safe, as we don't expose live links to user
-  this.oldest = this.newest = undefined;
-  this.size = 0;
-  this._keymap.clear();
-};
-
-
-function EntryIterator(oldestEntry) { this.entry = oldestEntry; }
-EntryIterator.prototype[Symbol.iterator] = function() { return this; }
-EntryIterator.prototype.next = function() {
-  let ent = this.entry;
-  if (ent) {
-    this.entry = ent[NEWER];
-    return { done: false, value: [ent.key, ent.value] };
-  } else {
-    return { done: true, value: undefined };
-  }
-};
-
-
-function KeyIterator(oldestEntry) { this.entry = oldestEntry; }
-KeyIterator.prototype[Symbol.iterator] = function() { return this; }
-KeyIterator.prototype.next = function() {
-  let ent = this.entry;
-  if (ent) {
-    this.entry = ent[NEWER];
-    return { done: false, value: ent.key };
-  } else {
-    return { done: true, value: undefined };
-  }
-};
-
-function ValueIterator(oldestEntry) { this.entry = oldestEntry; }
-ValueIterator.prototype[Symbol.iterator] = function() { return this; }
-ValueIterator.prototype.next = function() {
-  let ent = this.entry;
-  if (ent) {
-    this.entry = ent[NEWER];
-    return { done: false, value: ent.value };
-  } else {
-    return { done: true, value: undefined };
-  }
-};
-
-
-LRUMap.prototype.keys = function() {
-  return new KeyIterator(this.oldest);
-};
-
-LRUMap.prototype.values = function() {
-  return new ValueIterator(this.oldest);
-};
-
-LRUMap.prototype.entries = function() {
-  return this;
-};
-
-LRUMap.prototype[Symbol.iterator] = function() {
-  return new EntryIterator(this.oldest);
-};
-
-LRUMap.prototype.forEach = function(fun, thisObj) {
-  if (typeof thisObj !== 'object') {
-    thisObj = this;
-  }
-  let entry = this.oldest;
-  while (entry) {
-    fun.call(thisObj, entry.value, entry.key, this);
-    entry = entry[NEWER];
-  }
-};
-
-/** Returns a JSON (array) representation */
-LRUMap.prototype.toJSON = function() {
-  var s = new Array(this.size), i = 0, entry = this.oldest;
-  while (entry) {
-    s[i++] = { key: entry.key, value: entry.value };
-    entry = entry[NEWER];
-  }
-  return s;
-};
-
-/** Returns a String representation */
-LRUMap.prototype.toString = function() {
-  var s = '', entry = this.oldest;
-  while (entry) {
-    s += String(entry.key)+':'+entry.value;
-    entry = entry[NEWER];
-    if (entry) {
-      s += ' < ';
+        next() {
+            let ent = this.entry;
+            if (ent) {
+                this.entry = ent[NEWER];
+                return {done: false, value: [ent.key, ent.value]};
+            } else {
+                return {done: true, value: undefined};
+            }
+        }
     }
-  }
-  return s;
-};
 
+    EntryIterator.prototype[Symbol.iterator] = function () {
+        return this;
+    };
+
+    class KeyIterator {
+        constructor(oldestEntry) {
+            this.entry = oldestEntry;
+        }
+
+        next() {
+            let ent = this.entry;
+            if (ent) {
+                this.entry = ent[NEWER];
+                return {done: false, value: ent.key};
+            } else {
+                return {done: true, value: undefined};
+            }
+        }
+    }
+
+    KeyIterator.prototype[Symbol.iterator] = function () {
+        return this;
+    };
+
+    class ValueIterator {
+        constructor(oldestEntry) {
+            this.entry = oldestEntry;
+        }
+
+        next() {
+            let ent = this.entry;
+            if (ent) {
+                this.entry = ent[NEWER];
+                return {done: false, value: ent.value};
+            } else {
+                return {done: true, value: undefined};
+            }
+        }
+    }
+
+    ValueIterator.prototype[Symbol.iterator] = function () {
+        return this;
+    };
 });
